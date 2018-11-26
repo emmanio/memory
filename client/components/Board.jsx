547a5e5774e91d2dd5f89bc476c278bf1cd0a25f
emma-nio <emmanioashley@gmail.com>
2018-11-26 18:29:59 +1300

    added tile2 and checkpair function, if both tiles have been selected their values are passed to evalMatch as props. If it's a match then we count +1, if not setTimeout is for 1second and tiles are hidden

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -6,14 +6,33 @@ class Board extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      tile1: null
+      tile1: null,
+      tile2: null
     }
+    this.checkPair = this.checkPair.bind(this)
     this.onClick = this.handleClick.bind(this)
   }
 
   handleClick (id) {
-    const tile = this.props.find(tile => tile.id === id)
+    const {tile1} = this.state
+    const tile = this.props.tiles.find(tile => tile.id === id)
     tile.isVisible = true
+
+    tile1 === null ? this.setState({tile1: tile}) : this.setState({tile2: tile}, this.checkPair)
+  }
+
+  checkPair () {
+    const {tile1, tile2} = this.state
+    const isMatch = tile1.value === tile2.value
+
+    const checkMatch = () => {
+      this.setState({
+        tile1: null,
+        tile2: null
+      })
+      this.props.evalMatch(tile1, tile2)
+    }
+    isMatch ? checkMatch() : setTimeout(checkMatch, 1000)
   }
 
   render () {
@@ -24,7 +43,7 @@ class Board extends React.Component {
           info={tile.info}
           value={tile.value}
           isVisible={tile.isVisible}
-          onClick={this.handleClick.bind(tile.id)}/>
+          onClick={this.handleClick.bind(this, tile.id)}/>
       })}
     </div>
   }
