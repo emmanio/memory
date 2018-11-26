c18c1672e6b98387de76b1fc4e37b36838f93738
emma-nio <emmanioashley@gmail.com>
2018-11-26 18:31:24 +1300

    added reset function which hides all the tiles and sets state back to default values, evalMatch function evaluates tile1 and tile2, if they are a match then we count +1 to matchCount, otherwise tile is set to false and hidden

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,7 +9,41 @@ const winMessage = 'Congratulations, you matched all the tiles!'
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      matchCount: 0,
+      isMatch: true,
+      tiles: startingTiles
+    }
+    this.reset = this.reset.bind(this)
+    this.evalMatch = this.evalMatch.bind(this)
+  }
+
+  reset () {
+    const newTiles = startingTiles.map(tile => {
+      tile.isVisible = false
+      return tile
+    })
+    this.setState({
+      matchCount: 0,
+      isMatch: true,
+      tiles: newTiles
+    })
+  }
+
+  evalMatch (tile1, tile2) {
+    const {matchCount} = this.state
+    const isMatch = tile1.value === tile2.value
+    const updatedTiles = this.state.tiles.map(tile => {
+      if (!isMatch && (tile === tile1 || tile === tile2)) {
+        tile.isVisible = false
+      }
+      return tile
+    })
+    this.setState({
+      matchCount: isMatch ? matchCount + 1 : matchCount,
+      tiles: updatedTiles,
+      isMatch: isMatch
+    })
   }
 
   render () {
@@ -19,7 +53,7 @@ class App extends React.Component {
         <h1>Welcome to the Memory Game</h1>
         <h2>Match all the tiles to win</h2>
 
-        <Board tiles={startingTiles} foundMatch={this.foundMatch} />
+        <Board tiles={startingTiles} evalMatch={this.evalMatch} />
 
         <h5>{hasWon && winMessage}</h5>
         <h5>{!this.state.isMatch && tryAgain}</h5>
